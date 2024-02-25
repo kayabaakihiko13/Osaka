f0506668f805531df5bfb94e3e673271ce5d5fff
kayabaakihiko13 <iqbalramad75@gmail.com>
2024-02-26 02:36:57 +0700

    feat: fix bugs busniess id

--- Osaka/api.py
+++ Osaka/api.py
@@ -53,7 +53,8 @@ class GoogleMapAPI(object):
             return pd.json_normalize(reviews) if reviews else pd.DataFrame()
 
         elif isinstance(business_id, pd.Series):
-            all_results = {}
+            all_results = []
+
             for id in business_id:
                 querystring = {
                     "business_id": id,
@@ -69,7 +70,9 @@ class GoogleMapAPI(object):
                     reviews = data[0].get("reviews", [])
                 else:
                     reviews = data.get("data", {}).get("reviews", [])
-                result_data = pd.json_normalize(reviews) if reviews else pd.DataFrame()
-                all_results[id] = result_data.to_dict(orient="records")
 
-            return pd.DataFrame.from_dict(all_results, orient="index")
+                result_data = pd.json_normalize(reviews) if reviews else pd.DataFrame()
+                result_data["business_id"] = id
+                all_results.append(result_data)
+
+            return pd.concat(all_results, ignore_index=True)
